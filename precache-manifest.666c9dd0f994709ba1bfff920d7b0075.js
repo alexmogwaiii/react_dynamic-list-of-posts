685ee9d8e81a7f5545e2b1d85d18c29c959c5cc9
Alex <alexmogwaiii@gmail.com>
2021-02-01 23:29:55 +0200

    Updates

---
+++ precache-manifest.666c9dd0f994709ba1bfff920d7b0075.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda904bff2eb2503d204b020ccb52f5e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ec74e3cb79b109df5bc0",
+    "url": "./static/css/main.bfcdaf66.chunk.css"
+  },
+  {
+    "revision": "2a715f52c9545ef10f01",
+    "url": "./static/js/2.cae640d0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cae640d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec74e3cb79b109df5bc0",
+    "url": "./static/js/main.7f94a57f.chunk.js"
+  },
+  {
+    "revision": "93b65d15acd366da5855",
+    "url": "./static/js/runtime-main.2badbef3.js"
+  }
+]);
